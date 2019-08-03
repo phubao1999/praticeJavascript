a20bbaae3cc3416056d1803c935da7451c0ab5b8
phubao <phubao1999>
2019-08-04 04:01:50 +0700

    Promise.All

--- 33-promise/index.js
+++ 33-promise/index.js
@@ -21,10 +21,11 @@
 //     .catch(onError);
 
 var fs = require('fs');
+const axios = require('axios');
 
 function readFilePromise(path) {
     return new Promise(function (resolve, reject) {
-        fs.readFile(path, { encoding: 'utf8' }, function (err, data) {
+        fs.readFile(path, { encoding: 'utf8' }, (err, data) => {
             if(err){
                 reject(err);
             } else {
@@ -34,18 +35,30 @@ function readFilePromise(path) {
     })
 }
 
-readFilePromise('song1.txt')
-    .then((song1) => {
-        console.log(song1);
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+// readFilePromise('song1.txt')
+//     .then((song1) => {
+//         console.log(song1);
+//         return readFilePromise('song2.txt');
+//     })
+//     .then((song2) => {
+//         console.log(song2);
+//         return readFilePromise('song3.txt');
+//     })
+//     .then((song3)=> {
+//         console.log(song3);
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });
 
-readFilePromise('song2.txt')
-    .then((song2) => {
-        console.log(song2);
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+Promise.all([
+    readFilePromise('song1.txt'),
+    readFilePromise('song2.txt'),
+    readFilePromise('song3.txt')
+]).then( (values) => {
+    console.log(values);
+}).catch((err) => {
+    console.log(err);
+})
+
+    
